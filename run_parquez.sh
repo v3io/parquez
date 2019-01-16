1df3be768b219be51a35466c156ae2c5a9ce5432
aviaIguazio <avia@iguazio.com>
2019-01-16 13:57:52 +0200

    ordering scripts

--- run_parquez.sh
+++ run_parquez.sh
@@ -8,4 +8,6 @@ if ! [ -d venv  ] ; then
 else
         source venv/bin/activate
 fi
-python core/parquez.py ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14}
+
+python parquez.py ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14}
+
