76e575e61208b0363118ad02396c8b44ec6889e7
aviaIguazio <avia@iguazio.com>
2019-02-03 14:58:45 +0200

    updating alter kv view

--- core/alter_kv_view.py
+++ core/alter_kv_view.py
@@ -3,10 +3,10 @@ from logger import Logger
 from kv_table import KVTable
 from kv_view import KVView
 
-CONFIG_PATH = 'config/test_config.ini'
+CONFIG_PATH = 'config/config.ini'
 
 
-def parse_kv_window_arg(val,logger):
+def parse_kv_window_arg(val, logger):
     val = val.replace('hours', 'h')
     val = val.replace('days', 'd')
     val = val.replace(" ", "")
@@ -23,10 +23,10 @@ def main():
     conf = AppConf(logger, CONFIG_PATH)
     conf.log_conf()
 
-    logger.info("validating kv table "+args[1])
+    logger.info("validating kv table " + args[1])
     kv_table = KVTable(conf, args[1], logger)
 
-    logger.info("generating view over kv"+args[2])
+    logger.info("generating view over kv" + args[2])
     parsed_window = parse_kv_window_arg(args[2], logger)
     kv_view = KVView(logger, parsed_window, conf, kv_table)
     kv_view.generate_crete_view_script()
@@ -35,7 +35,8 @@ def main():
 if __name__ == '__main__' and __package__ is None:
     if __package__ is None:
         from os import path
-        sys.path.append( path.dirname(path.dirname( path.abspath(__file__) ) ) )
+
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
         from config.app_conf import AppConf
     else:
         from ..config.app_conf import AppConf
