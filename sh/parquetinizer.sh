7638051784e6377887f5c7aa5506061a083fb537
aviaIguazio <avia@iguazio.com>
2019-02-07 11:19:12 +0200

    updating alter view command

--- sh/parquetinizer.sh
+++ sh/parquetinizer.sh
@@ -230,7 +230,11 @@ popd
 
 pushd $parquez_dir
 
-${parquez_dir}/sh/alter_kv_view.sh "${kv_table_name}" "${kv_window}"
+alter_view_command="${parquez_dir}/sh/alter_kv_view.sh ${kv_table_name} '${kv_window}'"
+
+ehco ${alter_view_command}
+
+eval ${alter_view_command}
 
 #/opt/hive/bin/hive -e "alter table $hive_schema.$parquet_table_name add partition (year=$year, month=$month, day=$day, hour=$hour) location '$target';"
 ${parquez_dir}/sh/hive_partition.sh add $hive_schema $parquet_table_name $year $month $day $hour ${target} $partition_by
