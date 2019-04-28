819a1b29a66910e3df8c423d04902f3463a9c11c
aviaIguazio <avia@iguazio.com>
2019-04-28 18:18:54 +0300

    adding presto cli

--- core/parquet_table.py
+++ core/parquet_table.py
@@ -52,7 +52,7 @@ class ParquetTable(object):
         os.system(command)
 
     def copy_to_v3io(self):
-        'curl https: //' + self.conf.v3io_api_endpoint_host + '/parquez/ -H \'x-v3io-session-key: ' + self.conf.v3io_access_key + '\' --insecure --upload-file create_table.txt'
+        'curl https: //' + self.conf.v3io_api_endpoint_host + '/parquez/ -H \'x-v3io-session-key: ' + self.conf.v3io_access_key + '\' --insecure --upload-file create_table.txt"'
 
     def generate_script(self):
         try:
