6efa42df1a45211c177594cf3bd7c996eee2957c
aviaIguazio <avia@iguazio.com>
2019-04-29 10:45:39 +0300

    adding presto cli

--- parquez.py
+++ parquez.py
@@ -44,7 +44,7 @@ def main():
     #prest.execute_command()
 
     logger.info("generating presto view")
-    prest = PrestoClient(logger, args.view_name, args.partition_by, conf, kv_table, kv_view)
+    prest = PrestoClient(logger, conf, parquet, kv_view)
     prest.generate_unified_view()
 
 
