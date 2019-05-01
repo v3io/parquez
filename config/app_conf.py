7e377e9768463d990c0bc3be400e30bd53cd4d8a
aviaIguazio <avia@iguazio.com>
2019-05-01 16:29:33 +0300

    removing unnecessary attributes in app_conf

--- config/app_conf.py
+++ config/app_conf.py
@@ -10,9 +10,7 @@ class AppConf(object):
         config.read(config_path)
         self.logger.debug(os.getcwd())
         self.v3io_container = config['v3io']['v3io_container']
-        self.v3io_path = config['v3io']['v3io_path']
         self.v3io_access_key = config['v3io']['access_key']
-        self.hive_home = config['hive']['hive_home']
         self.hive_schema = config['hive']['hive_schema']
         self.v3io_connector = config['presto']['v3io_connector']
         self.hive_connector = config['presto']['hive_connector']
@@ -21,9 +19,6 @@ class AppConf(object):
         self.v3io_api_endpoint_port = config['nginx']['v3io_api_endpoint_port']
         self.username = config['nginx']['username']
         self.password = config['nginx']['password']
-
-    def log_conf(self):
-        self.logger.info(self.hive_home)
 
     def presto_v3io_prefix(self):
         return self.v3io_connector+"."+self.v3io_container
