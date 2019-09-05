aea2658cc18bf35e80ac20e5f1ba6b185a877560
aviaIguazio <avia@iguazio.com>
2019-09-05 14:50:20 +0300

    add logging to utils copy command

--- utils/utils.py
+++ utils/utils.py
@@ -8,4 +8,5 @@ class Utils(object):
 
     def copy_to_v3io(self, file):
         command = 'curl https://' + self.conf.v3io_api_endpoint_host + '/' + self.conf.v3io_container + '/ -H \'x-v3io-session-key: ' + self.conf.v3io_access_key + '\' --insecure --upload-file ' + file
+        self.logger.info('copy command %s ' % command)
         os.system(command)
