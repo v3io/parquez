2d26c389783b6968ff300e591a84b71304de83e3
aviaIguazio <avia@iguazio.com>
2020-06-23 11:49:05 +0300

    adding create table parquet

---
+++ create_parquet_table.py
@@ -0,0 +1,32 @@
+from mlrun import get_or_create_ctx
+from core.kv_table import KVTable
+from config.app_conf import AppConf
+from core.parquet_table import ParquetTable
+from utils.utils import Utils
+from core.k8s_client import K8SClient
+
+CONFIG_PATH = 'User/parquez/config/parquez.ini'
+REAL_TIME_TABLE_NAME = 'faker'
+PARTITION_BY = 'h'
+
+
+
+def main(context, partition_by):
+    context.logger.info("loading configuration")
+    conf = AppConf(context.logger, CONFIG_PATH)
+
+    utils = Utils(context.logger, conf)
+
+    kv_table = KVTable(context.logger, conf, REAL_TIME_TABLE_NAME)
+    schema = kv_table.import_table_schema()
+
+    context.logger.info("logging schema")
+    context.log_artifact('schema', body=schema, local_path='schema.txt')
+
+    context.logger.info("generating parquet table")
+    parquet = ParquetTable(context.logger, conf, utils, partition_by, kv_table, K8SClient(context.logger))
+    parquet.generate_script()
+
+if __name__ == '__main__':
+    context = get_or_create_ctx('get-schema')
+    main(context,PARTITION_BY)
