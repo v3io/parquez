0197471c24cde8b95c80acf4f5e73e7786f72523
aviaIguazio <avia@iguazio.com>
2020-07-26 16:28:57 +0300

    adding create container

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,13 +4,20 @@ from utils.utils import Utils
 
 
 # test_utils.py
-def test_utils():
+def test_copy_v3io_file():
     logger = Logger()
     logger.info("logging......")
     conf = AppConf(logger, "test.ini")
     utils = Utils(logger, conf)
     utils.copy_to_v3io("../v3io-spark2-tools_2.11.jar")
 
+
+def test_list_containers():
+    logger = Logger()
+    logger.info("logging......")
+    conf = AppConf(logger, "test.ini")
+    utils = Utils(logger, conf)
+    utils.list_containers()
 
 
 
