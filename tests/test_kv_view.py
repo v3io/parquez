150835fdb073d91603a6ffa5c35d61e56bfa7571
Avi Asulin <avia@iguazio.com>
2021-01-31 16:01:18 +0200

    update kv_view to use presto

--- tests/test_kv_view.py
+++ tests/test_kv_view.py
@@ -1,18 +1,17 @@
 from utils.logger import Logger
 from config.app_conf import AppConf
-from core.kv_table import KVTable
 from core.kv_view import KVView
 from core.params import Params
+from core.presto_client import PrestoClient
 
 PARAMS = Params(partition_by='h', real_time_table_name="faker", user_name='avia',
-                access_key='c8595589-097a-496d-8a46-e5dc3689ee37')
+                access_key='051b75cc-019b-49da-83cc-c17495ed6d99')
 
 
 def test_kv_view():
     logger = Logger()
     conf = AppConf(logger, "test.ini")
     params = PARAMS
-    kv_view = KVView(logger, params, conf)
+    presto_client = PrestoClient(logger, conf, params)
+    kv_view = KVView(logger, params, conf, presto_client)
     kv_view.generate_crete_view_script()
-
-
