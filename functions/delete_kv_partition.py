9fd533ff715a530a9138b895b11c737c04dbeb3b
aviaIguazio <avia@iguazio.com>
2021-02-16 12:32:16 +0000

    update delete kv partition

--- functions/delete_kv_partition.py
+++ functions/delete_kv_partition.py
@@ -4,12 +4,13 @@ def main(context):
     partition = context.parameters['kv_path']
     context.logger.info("deleting partition {}".format(partition))
     sj = new_function(kind='spark',
-                      command='/igz/java/libs/v3io-spark2-tools_2.11.jar',
+                      #command='/igz/java/libs/v3io-spark2-tools_2.11.jar',
+                      command='/igz/java/libs/v3io-spark2-tools_2.11-7098927429497484201.jar',
                       name='spark_delete_kv_partition')
-    sj.with_driver_limits(cpu="1300m")
-    sj.with_driver_requests(cpu=1, mem="512m")
-    sj.with_executor_limits(cpu="1400m")
-    sj.with_executor_requests(cpu=1, mem="512m")
+    sj.with_driver_limits(cpu="4000m")
+    sj.with_driver_requests(cpu=2, mem="512m")
+    sj.with_executor_limits(cpu="4000m")
+    sj.with_executor_requests(cpu=2, mem="512m")
     # Not really needed in this case
     sj.with_igz_spark()
     sj.spec.replicas = 2
